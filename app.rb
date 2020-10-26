6802d9e0f1d5c4ae9b8653198248134f278b3f45
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-26 01:14:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/' do
+      erb :root
+    end 
+    
+    get '/new'
+      erb :new
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new()
+      erb :show
+    end
+    
+    
 
   end
 end
